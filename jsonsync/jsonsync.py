8a835c083cc9ed0d2ea38db8ff802bc17c958cd7
Christopher Su <christophersu9@gmail.com>
2015-02-22 14:48:39 -0800

    more

--- jsonsync/jsonsync.py
+++ jsonsync/jsonsync.py
@@ -11,3 +11,15 @@ def sync(json_filename, data):
 
     with open(json_filename, 'w') as outfile:
         json.dump(json_data, outfile)
+
+# Syncs a JSON file with a JSON string
+def sync_json(json_filename, data):
+    sync(json_filename, json.loads(data))
+
+# Syncs a JSON file with another JSON file
+def sync_json(json_filename, other_filename):
+    other_file = open(other_filename)
+    other_data = json.load(other_file)
+    other_file.close()
+
+    sync(json_filename, other_data)
