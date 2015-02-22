3e1eb905dd5075ecdcf465efe4939d2f33118f8f
Christopher Su <christophersu9@gmail.com>
2015-02-22 14:46:08 -0800

    progress

--- jsonsync/jsonsync.py
+++ jsonsync/jsonsync.py
@@ -2,4 +2,12 @@ import json
 
 # Syncs a JSON file with a Python dictionary
 def sync(json_filename, data):
-    print "do stuff"
+    json_file = open(json_filename)
+    json_data = json.load(json_file)
+    json_file.close()
+
+    for key in data:
+        json_data[key] = data[key]
+
+    with open(json_filename, 'w') as outfile:
+        json.dump(json_data, outfile)
